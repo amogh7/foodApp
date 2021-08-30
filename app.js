1e6a2d9116914bbc4e1cf9e44cc6dd2160aa6096
Amogh7 <amoghsachdeva3@gmail.com>
2021-08-30 16:29:24 +0530

    removed procfile

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 
-app.use(express.static(__dirname+"/public"));
+app.use(express.static("public"));
 // app.use(function(req,res,next){
 //     console.log("I am called after express.json");
 // });
