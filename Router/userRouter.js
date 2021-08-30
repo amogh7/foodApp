de6f5e153c9bfe7a68d844f93d77cb2eb6129842
Amogh7 <amoghsachdeva3@gmail.com>
2021-08-30 16:49:44 +0530

    changed the path for public folder

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -7,7 +7,7 @@ const multer=require("multer");
 
 const storage = multer.diskStorage({
     destination: function(req , file , cb){
-      cb(null , "public/images/users")
+      cb(null , "Public/images/users")
     } ,
     filename : function(req , file , cb){
       cb(null , `user${Date.now()}.jpg`);
