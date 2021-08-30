d9177a04f2427a0f3f0f9c9e6c593d6e0e6e47b6
Amogh7 <amoghsachdeva3@gmail.com>
2021-08-30 15:45:17 +0530

    added paths

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 
-app.use(express.static("public"));
+app.use(express.static(__dirname+"/public"));
 // app.use(function(req,res,next){
 //     console.log("I am called after express.json");
 // });
@@ -40,7 +40,7 @@ app.use(express.static("public"));
 //app.httpmethod(appRoute,cb function(req,res));
 
 app.set("view engine","pug");
-app.set("views","view");
+app.set("views",path.join(_dirname,"view"));
 
 app.use("/api/plans",PlanRouter); 
 app.use("/api/users",userRouter);
